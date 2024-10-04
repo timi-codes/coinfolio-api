7f92f079ccc13391e685b056336c00aa5d7fd47b
--replace <tejumoladavid@gmail.com>
2024-10-03 22:28:52 -0400

    fix code linting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 
 
 @Module({
--- src/database/seeds/1728003480404_first-seed.ts
+++ src/database/seeds/1728003480404_first-seed.ts
@@ -1,6 +1,4 @@
-import type { Kysely } from 'kysely'
-
-export async function seed(db: Kysely<any>): Promise<void> {
+export async function seed(): Promise<void> {
 	// seed code goes here...
 	// note: this function is mandatory. you must implement this function.
 }
